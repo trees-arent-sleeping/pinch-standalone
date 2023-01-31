7e7fd37dcfa282c06bed730de31b8de36191bf14
Sam <transferdata76@gmail.com>
2023-01-31 11:38:10 -0800

    created player object, deciding between more classes or more constructor arguments to determine object types

--- app.js
+++ app.js
@@ -14,10 +14,25 @@ salmonStill.src = "fishpics/salmonStill.png"
 c.fillRect(0, 0, canvas.width, canvas.height)
 // creating a class for everything I'm animating
 class gameObject {
-    // position and speed are given as an object for legibility during instantiation
+    // position and speed are given as an object for legibility during instantiation. I think I'll use the species argument to determine what is drawn over the object. if not I'll extend this class
     constructor({pos, speed}) {
         this.pos = pos
         this.speed = speed
+        this.species = species
+    }
+    // drawing the sprite or shapes at the object's position
+    draw() {
+        c.drawImage(crabStill, this.pos.x, this.pos.y)
     }
 }
-
+// instantiating gameObject to make the player object
+const player = new gameObject({
+    pos: {
+        x: 256,
+        y: 256
+    },
+    speed: {
+        x: 0,
+        y: 0
+    }
+}, crab)
